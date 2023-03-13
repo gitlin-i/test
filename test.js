2e007c9579c286986da57819039f6341aaa6359c
jin <rkans30@gmail.com>
2023-03-13 17:00:39 +0900

    test:

---
+++ test.js
@@ -0,0 +1,7 @@
+var http = require("http")
+
+http.createServer(function(request, response){
+    response.writeHead(200,{'Content-Type':'text/plain'})
+    response.end('HelloWorld \n')
+}).listen(3000)
+console.log("Server running")
